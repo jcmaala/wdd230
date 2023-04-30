d3c9c78d98e28e00cc4fbed56edd0530059fe67c
jcmaala <jcmaala27@gmail.com>
2023-04-30 17:39:18 +0800

    Finished week2 assignment.

---
+++ lesson2/date.js
@@ -0,0 +1,14 @@
+const date = new Date();
+const todayDate = document.querySelector(".Date");
+
+function printingDateTime(){
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth()+1;
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+    todayDate.innerHTML = "Last updated: " + month + "/" + day + "/" + year + " " + hour + ":" + min + ":" + sec;
+}
+printingDateTime();
