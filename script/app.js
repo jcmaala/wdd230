2f37927a7a437fe9f07a8ad7cd88a8244a4b000f
jcmaala <jcmaala27@gmail.com>
2023-05-05 15:12:52 +0800

    changes made for assignment portal

--- script/app.js
+++ script/app.js
@@ -1,5 +1,6 @@
 const date = new Date();
 const todayDate = document.querySelector(".Date");
+const todayCopy = document.querySelector(".CopyrightYear");
 
 function printingDateTime(){
     const date = new Date();
@@ -12,5 +13,13 @@ function printingDateTime(){
     todayDate.innerHTML = "Last updated: " + month + "/" + day + "/" + year + " " + hour + ":" + min + ":" + sec;
 }
 printingDateTime();
+
+function printCopyRightYear(){
+    const year = date.getFullYear();
+    const theYear = "&#169; year | Jean Clyde Maala | Philippines";
+    const newYear = theYear.replace(/year/g, year);
+    todayCopy.innerHTML = newYear;
+}
+printCopyRightYear();
 
 
