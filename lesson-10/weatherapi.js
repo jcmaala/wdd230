b27a5ac075d234f0b1d9dd0b8c691137cb0f790c
jcmaala <jcmaala27@gmail.com>
2023-07-01 13:03:29 +0800

    added weather API assignment

---
+++ lesson-10/weatherapi.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=39.87&lon=-83.08&units=imperial&appid=a4110c7f8fb0f9899752f45db399315f";
+
+async function apiFetch(){
+    try{
+        const response = await fetch(apiURL);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+function displayResults(weatherData){
+    currentTemp.textContent = weatherData.main.temp;
+
+    const imgsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const description = weatherData.weather[0].description;
+    weatherIcon.setAttribute("src", imgsrc);
+    weatherIcon.setAttribute("alt", description);
+    captionDesc.innerHTML = description;
+}
+
+apiFetch();
