c8aa4728c69f32402185d8923541cfd6f35d09af
jcmaala <jcmaala27@gmail.com>
2023-07-02 07:05:13 +0800

    modified chamber of commerce site

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -10,7 +10,6 @@ async function apiFetch(){
         const response = await fetch(apiURL);
         if (response.ok){
             const data = await response.json();
-            //console.log(data);
             displayResults(data); 
         }
         else{
